aa09170973d75f3fbf5349ee5df7afebdbacae6a
Katherine Gacusan <gacusankatherine0514@gmail.com>
2024-03-06 19:28:36 -0800

    can view order by id

--- server.py
+++ server.py
@@ -6,11 +6,14 @@ from PizzaOrder import PizzaOrder
 
 app = Flask(__name__)
 
+
 def check_login(email, password):
     return email and password
 
+
 def sort_by_date(order):
-    return order['date'] 
+    return order["date"]
+
 
 @app.route("/")
 def home():
@@ -20,6 +23,7 @@ def home():
         orders = json.load(file)
         orders_sorted = sorted(orders, key=sort_by_date)
         return render_template("sortedorders.html", orders=orders_sorted)
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -33,7 +37,8 @@ def login():
             session["email"] = email
         else:
             return redirect(url_for("login"))
-        
+
+
 @app.route("/create", methods=["GET", "POST"])
 def create():
     # if "login" not in session:
@@ -45,25 +50,24 @@ def create():
         email = request.form.get("email")
         password = request.form.get("password")
         role = request.form.get("role")
-        
+
         users = []
         new_id = 1
-        if os.path.exists("./data/users.json") and os.path.getsize("./data/users.json") > 0:
+        if (
+            os.path.exists("./data/users.json")
+            and os.path.getsize("./data/users.json") > 0
+        ):
             with open("./data/users.json", "r") as file:
                 users = json.load(file)
             max_id = max(users, key=lambda x: x["id"])["id"]
             new_id = max_id + 1
-        user_info = {
-            "id": new_id,
-            "email": email,
-            "password": password,
-            "role": role
-        }
+        user_info = {"id": new_id, "email": email, "password": password, "role": role}
         users.append(user_info)
         with open("./data/users.json", "w") as file:
             json.dump(users, file, indent=4)
         return redirect(url_for("home"))
-    
+
+
 @app.route("/pizza", methods=["GET", "POST"])
 def pizza():
     # if "login" not in session:
@@ -78,10 +82,13 @@ def pizza():
         quantity = request.form.get("quantity")
         price = request.form.get("price")
         date = request.form.get("date")
-        
+
         orders = []
         pizza_id = 1
-        if os.path.exists("./data/pizzaorders.json") and os.path.getsize("./data/pizzaorders.json") > 0:
+        if (
+            os.path.exists("./data/pizzaorders.json")
+            and os.path.getsize("./data/pizzaorders.json") > 0
+        ):
             with open("./data/pizzaorders.json", "r") as file:
                 orders = json.load(file)
             max_id = max(orders, key=lambda x: x["id"])["id"]
@@ -93,18 +100,29 @@ def pizza():
             "size": size,
             "quantity": quantity,
             "price": price,
-            "date": date
+            "date": date,
         }
         orders.append(order_info)
         with open("./data/pizzaorders.json", "w") as file:
             json.dump(orders, file, indent=4)
         return redirect(url_for("home"))
-    
+
+@app.route("/pizza/<int:id>")
+def get_pizza(id):
+    with open("./data/pizzaorders.json") as file:
+        orders = json.load(file)
+        for order in orders:
+            if order["id"] == id:
+                return render_template("order.html", order=order)
+        return redirect(url_for("home"))
+
+
 @app.route("/logout/")
 def logout():
     if "login" in session:
         session.pop("login")
     return redirect(url_for("login"))
-    
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=8888)
