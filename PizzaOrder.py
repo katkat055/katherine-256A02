1c175d7f05c76fad6ef637bc6e2a5a3973e9aede
Katherine Gacusan <gacusankatherine0514@gmail.com>
2024-03-15 10:52:09 -0700

    pizzaorder getting choices from json

--- PizzaOrder.py
+++ PizzaOrder.py
@@ -1,10 +1,22 @@
-from wtforms import DateField, FloatField, Form, IntegerField, RadioField, SelectField, StringField, SubmitField
+import json
+from wtforms import (
+    DateField,
+    FloatField,
+    Form,
+    IntegerField,
+    SelectField,
+    SubmitField,
+)
 from wtforms.validators import NumberRange
 
+with open("./data/init.json") as file:
+    options = json.load(file)
+
+
 class PizzaOrder(Form):
-    pizzatype = SelectField("Type", choices=["Canadian", "Cheese", "Hawaiian", "Meat Lovers", "Pepperoni", "Vegetarian"])
-    crust = SelectField("Crust", choices=["Cauliflower", "Deep Dish", "Regular", "Thin Crust"])
-    size = SelectField("Size", choices=["Individual", "Small", "Medium", "Large"])
+    pizzatype = SelectField("Type", choices=options["type"])
+    crust = SelectField("Crust", choices=options["crust"])
+    size = SelectField("Size", choices=options["size"])
     quantity = IntegerField("Quantity", validators=[NumberRange(min=1, max=10)])
     price = FloatField("Price")
     date = DateField("Date")
